2f246a6c1c44ee5f74754e21dfa287e80aeabd8e
Tran <pham.tr@northeastern.edu>
2021-03-14 18:02:07 -0400

    move mongo file to "services"

---
+++ services/MongoDatabase.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+const url = "mongodb://localhost/coveyuserdatabase";
+// Atlas URL  - replace UUU with user, PPP with password, XXX with hostname
+const url =
+  "mongodb+srv://testUser:P@123456@coveytownusers.w8gis.mongodb.net/test";
+
+async function connectToDb() {
+  const user = new MongoClient(url, { useNewUrlParser: true });
+  await user.connect();
+  console.log("Connected to MongoDB at", url);
+  db = user.db();
+}
+
+(async function () {
+  try {
+    await connectToDb();
+    app.listen(3000, function () {
+      console.log("App started on port 3000");
+    });
+  } catch (err) {
+    console.log("ERROR:", err);
+  }
+})();
