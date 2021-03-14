955a1b41d03cc6cf96f7261c1e8e0af99c7ef727
Tran <pham.tr@northeastern.edu>
2021-03-14 18:01:32 -0400

    user connect to Mongo, move mongo file to "services"

---
+++ services/roomService/src/router/MongoDatabase.js
@@ -0,0 +1,4 @@
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost/coveyuserdatabase';
+// Atlas URL  - replace UUU with user, PPP with password, XXX with hostname
+const url = 'mongodb+srv://testUser:P@123456@coveytownusers.w8gis.mongodb.net/test';
